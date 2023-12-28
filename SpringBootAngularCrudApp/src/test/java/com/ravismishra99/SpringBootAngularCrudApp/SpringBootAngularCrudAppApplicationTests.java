cb1daf458b880b16519572c59f19723e196dc0f1
Ravi Shankar Mishra <150536398+ravismishra99@users.noreply.github.com>
2023-12-28 16:17:11 +0530

    Add files via upload
    Spring Boot Employees CRUD Operaions

---
+++ SpringBootAngularCrudApp/src/test/java/com/ravismishra99/SpringBootAngularCrudApp/SpringBootAngularCrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravismishra99.SpringBootAngularCrudApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngularCrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
